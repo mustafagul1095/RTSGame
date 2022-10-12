94300b3a5d84c34e32a28f9360924a621e6ebbfc
mustafagul1095 <58472295+mustafagul1095@users.noreply.github.com>
2022-10-12 19:54:27 +0300

    SyncVar Callbacks Lecture

--- RTSGame/Assets/Scripts/MyNetworkManager.cs
+++ RTSGame/Assets/Scripts/MyNetworkManager.cs
@@ -11,5 +11,7 @@ public class MyNetworkManager : NetworkManager
 
         MyNetworkPlayer player = conn.identity.GetComponent<MyNetworkPlayer>();
         player.SetDisplayName($"Player {numPlayers}");
+        Color randColor = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        player.SetDisplayColor(randColor);
     }
 }
--- RTSGame/Assets/Scripts/MyNetworkPlayer.cs
+++ RTSGame/Assets/Scripts/MyNetworkPlayer.cs
@@ -1,15 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
+using TMPro;
 using UnityEngine;
 
 public class MyNetworkPlayer : NetworkBehaviour
 {
-    [SyncVar] [SerializeField] private string displayName = "Missing Name";
+    [SerializeField] private TMP_Text displayNameText;
+    [SerializeField] private Renderer displayColoRenderer;
+    
+    [SyncVar(hook = nameof(HandleDisplayNameUpdated))] 
+    [SerializeField] private string displayName = "Missing Name";
+    
+    [SyncVar(hook = nameof(HandleDisplayColourUpdated))] 
+    [SerializeField] private Color displayColor = Color.black;
 
     [Server]
     public void SetDisplayName(string newDisplayName)
     {
         displayName = newDisplayName;
+    }
+    
+    [Server]
+    public void SetDisplayColor(Color newDisplayColor)
+    {
+        displayColor = newDisplayColor;
+    }
+
+    private void HandleDisplayColourUpdated(Color oldColor, Color newColor)
+    {
+        displayColoRenderer.material.SetColor("_BaseColor", newColor);
+    }
+
+    private void HandleDisplayNameUpdated(string oldName, string newName)
+    {
+        displayNameText.SetText(newName);
     }
 }
