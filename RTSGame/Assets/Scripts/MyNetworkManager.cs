71c108399841e2b957f1347b456624e604999e15
mustafagul1095 <58472295+mustafagul1095@users.noreply.github.com>
2022-10-12 16:34:50 +0300

    NetworkManager Lecture

---
+++ RTSGame/Assets/Scripts/MyNetworkManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class MyNetworkManager : NetworkManager
+{
+    public override void OnClientConnect()
+    {
+        base.OnClientConnect();
+        
+        Debug.Log("I connected to a server");
+    }
+
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        base.OnServerAddPlayer(conn);
+        
+        Debug.Log(numPlayers);
+    }
+}
