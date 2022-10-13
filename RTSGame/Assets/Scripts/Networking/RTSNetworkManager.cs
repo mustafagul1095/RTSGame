aca1f84455f789f4f94fb90dd2cada4fb8d79880
mustafagul1095 <58472295+mustafagul1095@users.noreply.github.com>
2022-10-13 23:30:57 +0300

    SpawnUnits

--- RTSGame/Assets/Scripts/Networking/RTSNetworkManager.cs
+++ RTSGame/Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -5,5 +5,16 @@ using UnityEngine;
 
 public class RTSNetworkManager : NetworkManager
 {
+    [SerializeField] private GameObject unitSpawnerPrefab;
 
+    public override void OnServerAddPlayer(NetworkConnectionToClient conn)
+    {
+        base.OnServerAddPlayer(conn);
+
+        GameObject unitSpawnerInstance = Instantiate(unitSpawnerPrefab, 
+            conn.identity.transform.position,
+            conn.identity.transform.rotation);
+        
+        NetworkServer.Spawn(unitSpawnerInstance, conn);
+    }
 }
