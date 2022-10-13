680b1bbd48b138308ea04ff0a71eda383cf38393
mustafagul1095 <58472295+mustafagul1095@users.noreply.github.com>
2022-10-13 14:28:48 +0300

    Unit Movement Lecture

---
+++ RTSGame/Assets/Scripts/Networking/RTSPlayerMover.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.InputSystem;
+
+public class RTSPlayerMover : NetworkBehaviour
+{
+    [SerializeField] private NavMeshAgent agent;
+
+    private Camera _mainCamera;
+
+    #region Server
+
+    [Command]
+    private void CmdMove(Vector3 position)
+    {
+        if(!NavMesh.SamplePosition(position, out NavMeshHit hit, 1f, NavMesh.AllAreas)){return;}
+        
+        agent.SetDestination(position);
+    }
+
+    #endregion
+
+    #region Client
+
+    public override void OnStartAuthority()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    [ClientCallback]
+    private void Update()
+    {
+        if(!hasAuthority){return;}
+        if(!Mouse.current.rightButton.wasPressedThisFrame){return;}
+
+        Ray ray = _mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if(!Physics.Raycast(ray,out  RaycastHit hit, Mathf.Infinity)){return;}
+        CmdMove(hit.point);
+    }
+
+    #endregion
+}
