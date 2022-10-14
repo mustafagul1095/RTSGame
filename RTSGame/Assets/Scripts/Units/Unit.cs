41c771f3f3aed8c5b7d833340f0ad2fa03ff23cc
mustafagul1095 <58472295+mustafagul1095@users.noreply.github.com>
2022-10-14 11:42:09 +0300

    StoringUnits Lecture

--- RTSGame/Assets/Scripts/Units/Unit.cs
+++ RTSGame/Assets/Scripts/Units/Unit.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -10,11 +12,45 @@ public class Unit : NetworkBehaviour
     [SerializeField] private UnityEvent onSelected;
     [SerializeField] private UnityEvent onDeselected;
 
+    public static event Action<Unit> ServerOnUnitSpawned;
+    public static event Action<Unit> ServerOnUnitDespawned;
+
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer()
+    {
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+
+    #endregion
+
     public UnitMover GetUnitMover()
     {
         return unitMover;
     }
+   
     #region Client
+
+    public override void OnStartClient()
+    {
+        if(!isClientOnly || !hasAuthority){return;}
+        
+        AuthorityOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopClient()
+    {
+        if(!isClientOnly || !hasAuthority){return;}
+        AuthorityOnUnitDespawned?.Invoke(this);
+    }
 
     [Client]
     public void Select()
