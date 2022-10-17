faaae445175c16958c8d130bd5130524e2a4699c
mustafagul1095 <58472295+mustafagul1095@users.noreply.github.com>
2022-10-17 16:13:26 +0300

    Damage Dealing

---
+++ RTSGame/Assets/Scripts/Combat/Health.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+
+public class Health : NetworkBehaviour
+{
+    [SerializeField] private int maxHealth = 100;
+
+    [SyncVar]
+    private int _currentHealth;
+
+    public event Action ServerOnDie;
+
+    #region Server
+
+    public override void OnStartServer()
+    {
+        _currentHealth = maxHealth;
+    }
+
+    [Server]
+    public void DealDamage(int damageAmount)
+    {
+        if(_currentHealth == 0){return;}
+
+        _currentHealth = Mathf.Max(_currentHealth - damageAmount, 0);
+        
+        if(_currentHealth != 0){return;}
+        
+        ServerOnDie?.Invoke();
+        
+        Debug.Log("We DIED");
+    }
+    
+    #endregion
+
+    #region Client
+
+    
+
+    #endregion
+}
