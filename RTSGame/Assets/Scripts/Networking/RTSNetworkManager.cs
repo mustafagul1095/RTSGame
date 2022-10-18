91684210059be3f7cc2378fd697b020993d58faa
mustafagul1095 <58472295+mustafagul1095@users.noreply.github.com>
2022-10-18 14:19:43 +0300

    GameOver

--- RTSGame/Assets/Scripts/Networking/RTSNetworkManager.cs
+++ RTSGame/Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using Mirror;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RTSNetworkManager : NetworkManager
 {
     [SerializeField] private GameObject unitSpawnerPrefab;
+    [SerializeField] private GameOverHandler gameOverHandlerPrefab;
 
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
@@ -16,5 +18,15 @@ public class RTSNetworkManager : NetworkManager
             conn.identity.transform.rotation);
         
         NetworkServer.Spawn(unitSpawnerInstance, conn);
+    }
+
+    public override void OnServerSceneChanged(string sceneName)
+    {
+        if (SceneManager.GetActiveScene().name.StartsWith("Scene_Map"))
+        {
+            GameOverHandler gameOverHandlerInstance = Instantiate(gameOverHandlerPrefab);
+            
+            NetworkServer.Spawn(gameOverHandlerInstance.gameObject);
+        }
     }
 }
