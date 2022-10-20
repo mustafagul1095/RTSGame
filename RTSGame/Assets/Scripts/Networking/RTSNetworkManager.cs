4489112b37dc30c15311ac0a1674c83b0e68da1a
mustafagul1095 <58472295+mustafagul1095@users.noreply.github.com>
2022-10-20 20:29:52 +0300

    Creating the Lobby

--- RTSGame/Assets/Scripts/Networking/RTSNetworkManager.cs
+++ RTSGame/Assets/Scripts/Networking/RTSNetworkManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
@@ -9,6 +10,23 @@ public class RTSNetworkManager : NetworkManager
     [SerializeField] private GameObject unitBasePrefab;
     [SerializeField] private GameOverHandler gameOverHandlerPrefab;
 
+    public static event Action ClientOnConnected;
+    public static event Action ClientOnDisconnected;
+
+    public override void OnClientConnect()
+    {
+        base.OnClientConnect();
+        
+        ClientOnConnected?.Invoke();
+    }
+
+    public override void OnClientDisconnect()
+    {
+        base.OnClientDisconnect();
+        
+        ClientOnDisconnected?.Invoke();
+    }
+
     public override void OnServerAddPlayer(NetworkConnectionToClient conn)
     {
         base.OnServerAddPlayer(conn);
@@ -17,12 +35,7 @@ public class RTSNetworkManager : NetworkManager
             UnityEngine.Random.Range(0f,1f),
             UnityEngine.Random.Range(0f,1f),
             UnityEngine.Random.Range(0f,1f)));
-
-        GameObject unitSpawnerInstance = Instantiate(unitBasePrefab, 
-            conn.identity.transform.position,
-            conn.identity.transform.rotation);
         
-        NetworkServer.Spawn(unitSpawnerInstance, conn);
     }
 
     public override void OnServerSceneChanged(string sceneName)
